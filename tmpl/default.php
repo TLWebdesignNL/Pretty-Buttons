64195199f51519c5bbf76b2f3c86049fce761e82
TLWebdesign <info@tlwebdesign.nl>
2022-08-02 00:07:27 +0200

    joomla code formatting applied

--- tmpl/default.php
+++ tmpl/default.php
@@ -10,22 +10,23 @@
 \defined('_JEXEC') or die;
 
 $wrapperClass = "";
-if ($block == 1) {
-    $wrapperClass = "d-grid gap-2";
+if ($block == 1)
+{
+	$wrapperClass = "d-grid gap-2";
 }
 ?>
 
 <div class="d-flex flex-column pretty-buttons">
-    <?php if ($before) : ?>
+	<?php if ($before) : ?>
         <div class="before">
-            <?php echo $before; ?>
+			<?php echo $before; ?>
         </div>
-    <?php endif; // if before ?>
-    <?php if (is_object($buttons)) : ?>
+	<?php endif; // if before ?>
+	<?php if (is_object($buttons)) : ?>
         <div class="<?php echo $wrapperClass; ?> <?php echo $customouterclass; ?> pb-button-div">
-            <?php foreach ($buttons as $button) :
-                if ($button->url && ($button->iconclass || $button->buttontext)) :
-                    ?>
+			<?php foreach ($buttons as $button) :
+				if ($button->url && ($button->iconclass || $button->buttontext)) :
+					?>
                     <a class="
                         <?php echo (isset($button->buttonclass) && is_array($button->buttonclass)) ? implode(" ", $button->buttonclass) : $button->buttonclass; ?>
                         <?php echo (isset($button->custombuttonclass)) ? $button->custombuttonclass : ''; ?>"
@@ -33,21 +34,21 @@ if ($block == 1) {
                        target="<?php echo (isset($button->buttontarget)) ? $button->buttontarget : "_blank"; ?>"
                        aria-label="<?php echo (isset($button->buttontext)) ? $button->buttontext : ''; ?>"
                     >
-                        <?php if (isset($button->iconclass)) : ?>
+						<?php if (isset($button->iconclass)) : ?>
                             <i class="<?php echo $button->iconclass; ?> <?php echo (isset($button->buttontext) && $button->buttontext != "") ? 'pe-2' : ''; ?>"></i>
-                        <?php endif; // if $button->iconclass
-                        ?>
-                        <?php echo (isset($button->buttontext)) ? $button->buttontext : ''; ?>
+						<?php endif; // if $button->iconclass
+						?>
+						<?php echo (isset($button->buttontext)) ? $button->buttontext : ''; ?>
                     </a>
-                <?php
-                endif; // if $button->buttontext
-            endforeach; // foreach $buttons as $button
-            ?>
+				<?php
+				endif; // if $button->buttontext
+			endforeach; // foreach $buttons as $button
+			?>
         </div>
-    <?php endif; // if is_object(buttons) ?>
-    <?php if ($after) : ?>
+	<?php endif; // if is_object(buttons) ?>
+	<?php if ($after) : ?>
         <div class="after">
-            <?php echo $after; ?>
+			<?php echo $after; ?>
         </div>
-    <?php endif; // if before?>
+	<?php endif; // if before?>
 </div>
